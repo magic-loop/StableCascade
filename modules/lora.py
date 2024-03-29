045b5e675fce9cdc3ee839f36a0ee4e3bb94acfd
Kevin Lee <kevin@magicloop.ai>
2024-03-28 18:40:24 -0700

    Add debug

--- modules/lora.py
+++ modules/lora.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class LoRA(nn.Module):
-    def __init__(self, layer, name='weight', rank=16, alpha=1):
+    def __init__(self, layer, name="weight", rank=16, alpha=1):
         super().__init__()
         weight = getattr(layer, name)
         self.lora_down = nn.Parameter(torch.zeros((rank, weight.size(1))))
@@ -24,17 +24,26 @@ class LoRA(nn.Module):
 
 def apply_lora(model, filters=None, rank=16):
     def check_parameter(module, name):
-        return hasattr(module, name) and not torch.nn.utils.parametrize.is_parametrized(module, name) and isinstance(
-            getattr(module, name), nn.Parameter)
+        return (
+            hasattr(module, name)
+            and not torch.nn.utils.parametrize.is_parametrized(module, name)
+            and isinstance(getattr(module, name), nn.Parameter)
+        )
 
     for name, module in model.named_modules():
+        print(name)
+        print(module)
         if filters is None or any([f in name for f in filters]):
             if check_parameter(module, "weight"):
                 device, dtype = module.weight.device, module.weight.dtype
-                torch.nn.utils.parametrize.register_parametrization(module, 'weight', LoRA(module, "weight", rank=rank).to(dtype).to(device))
+                torch.nn.utils.parametrize.register_parametrization(
+                    module, "weight", LoRA(module, "weight", rank=rank).to(dtype).to(device)
+                )
             elif check_parameter(module, "in_proj_weight"):
                 device, dtype = module.in_proj_weight.device, module.in_proj_weight.dtype
-                torch.nn.utils.parametrize.register_parametrization(module, 'in_proj_weight', LoRA(module, "in_proj_weight", rank=rank).to(dtype).to(device))
+                torch.nn.utils.parametrize.register_parametrization(
+                    module, "in_proj_weight", LoRA(module, "in_proj_weight", rank=rank).to(dtype).to(device)
+                )
 
 
 class ReToken(nn.Module):
@@ -42,7 +51,7 @@ class ReToken(nn.Module):
         super().__init__()
         assert indices is not None
         self.embeddings = nn.Parameter(torch.zeros(len(indices), 1280))
-        self.register_buffer('indices', torch.tensor(indices))
+        self.register_buffer("indices", torch.tensor(indices))
         self.enabled = True
 
     def forward(self, embeddings):
@@ -55,12 +64,17 @@ class ReToken(nn.Module):
 
 def apply_retoken(module, indices=None):
     def check_parameter(module, name):
-        return hasattr(module, name) and not torch.nn.utils.parametrize.is_parametrized(module, name) and isinstance(
-            getattr(module, name), nn.Parameter)
+        return (
+            hasattr(module, name)
+            and not torch.nn.utils.parametrize.is_parametrized(module, name)
+            and isinstance(getattr(module, name), nn.Parameter)
+        )
 
     if check_parameter(module, "weight"):
         device, dtype = module.weight.device, module.weight.dtype
-        torch.nn.utils.parametrize.register_parametrization(module, 'weight', ReToken(indices=indices).to(dtype).to(device))
+        torch.nn.utils.parametrize.register_parametrization(
+            module, "weight", ReToken(indices=indices).to(dtype).to(device)
+        )
 
 
 def remove_lora(model, leave_parametrized=True):
@@ -68,4 +82,6 @@ def remove_lora(model, leave_parametrized=True):
         if torch.nn.utils.parametrize.is_parametrized(module, "weight"):
             nn.utils.parametrize.remove_parametrizations(module, "weight", leave_parametrized=leave_parametrized)
         elif torch.nn.utils.parametrize.is_parametrized(module, "in_proj_weight"):
-            nn.utils.parametrize.remove_parametrizations(module, "in_proj_weight", leave_parametrized=leave_parametrized)
+            nn.utils.parametrize.remove_parametrizations(
+                module, "in_proj_weight", leave_parametrized=leave_parametrized
+            )
