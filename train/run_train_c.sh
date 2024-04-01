6e8de70d589d331a69333aaaf491b2ec8fac82ff
Kevin Lee <kevin@magicloop.ai>
2024-03-31 21:45:16 -0700

    Call correct script

--- train/run_train_c.sh
+++ train/run_train_c.sh
@@ -5,4 +5,4 @@ export PYTHONPATH="/home/kevin/repos/StableCascade:$PYTHONPATH"
 source venv/bin/activate
 
 # Set $NUM_TRAINERS in environment before running the script
-torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_TRAINERS train/train_c_lora.py configs/training/finetune_c_3b.yaml
+torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_TRAINERS train/train_c.py configs/training/finetune_c_3b.yaml
