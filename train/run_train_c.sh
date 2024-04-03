8b40073ac265a4753cfe008235c1b87927390711
Kevin Lee <kevin@magicloop.ai>
2024-04-02 23:35:51 -0700

    Add single process training launcher

--- train/run_train_c.sh
+++ train/run_train_c.sh
@@ -6,4 +6,9 @@ source venv/bin/activate
 
 # Set $NUM_TRAINERS in environment and uncomment torchrun to run distributed training
 # torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_TRAINERS train/train_c.py configs/training/finetune_c_3b.yaml
+
+# Run single GPU training
+export LOCAL_RANK=0
+export RANK=0
+export WORLD_SIZE=1
 python3 train/train_c.py configs/training/finetune_c_3b.yaml
